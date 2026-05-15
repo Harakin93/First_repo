001fe5de4067b700445eb04e3746fa6d6dc53cc6
Jakub <Hara93@gmail.com>
2026-05-15 16:14:18 +0200

    Zad 4_1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+class Main {
+
+    public static void main(String...args) {
+        StringList stringList = new StringList();
+        stringList.addCompaniesToTheList();
+        stringList.concatenation();
+    }
+}
